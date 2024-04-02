b575a7b2368ba88af84960b21f5e8ce08520a982
Angel Christian Coayo Font <acoayo-f@c2r8s4.42madrid.com>
2024-04-02 19:52:25 +0200

    ss

---
+++ libreria/ft_substr.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acoayo-f <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/02 19:12:16 by acoayo-f          #+#    #+#             */
+/*   Updated: 2024/04/02 19:52:03 by acoayo-f         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*str;
+	size_t	i;
+	size_t	large;
+
+	i = 0;
+	if (!s)
+		return (0);
+	large = ft_strlen(s);
+	if (start >= large)
+		return (0);
+	if (large >= start + len)
+		str = malloc((len + 1) * sizeof(char));
+	if (!str)
+		return (NULL);
+	while (str[i] <= len)
+	{
+		str[i] = s[start + i];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
