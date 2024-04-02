418d5a281b4301b35f093f7a3366471e60afe2bf
Angel Christian Coayo Font <acoayo-f@c2r8s4.42madrid.com>
2024-04-02 20:37:04 +0200

    sd

---
+++ libreria/ft_strjoin.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acoayo-f <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/02 19:53:16 by acoayo-f          #+#    #+#             */
+/*   Updated: 2024/04/02 20:35:59 by acoayo-f         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*str;
+	size_t	i;
+	size_t	j;
+
+	i = ft_strlen(s1) + ft_strlen(s2);
+	str = malloc((i + 1) * sizeof(char));
+	if (!str)
+		return (NULL);
+	j = 0;
+	while (s1[j] != '\0')
+	{
+		str[j] = s1[j];
+		j++;
+	}
+	i = 0;
+	while (s2[i] != '\0')
+	{
+		str[j] = s2[i];
+		i++;
+		j++;
+	}
+	str[j] = '\0';
+	return (str);
+}
