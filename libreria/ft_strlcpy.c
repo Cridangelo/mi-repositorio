1f4a8142fe557ba422fabffea7fa737d0c2e0a9a
Angel Christian Coayo Font <acoayo-f@c2r6s5.42madrid.com>
2024-04-04 20:44:23 +0200

    s

--- libreria/ft_strlcpy.c
+++ libreria/ft_strlcpy.c
@@ -5,22 +5,30 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: acoayo-f <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/03/12 20:32:13 by acoayo-f          #+#    #+#             */
-/*   Updated: 2024/03/18 16:25:16 by acoayo-f         ###   ########.fr       */
+/*   Created: 2024/04/04 20:41:17 by acoayo-f          #+#    #+#             */
+/*   Updated: 2024/04/04 20:42:10 by acoayo-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 #include "libft.h"
 
-size_t	ft_strlcpy(char *dest, const char *src, size_t size)
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 {
 	size_t	i;
+	size_t	x;
 
 	i = 0;
-	while (i < size - 1 && src[i] != '\0')
+	x = 0;
+	if (dstsize > 0)
 	{
-		dest[i] = src[i];
-		i++;
+		while (src[i] != '\0' && i < dstsize - 1)
+		{
+			dst[i] = src[i];
+			i++;
+		}
+		dst[i] = '\0';
 	}
-	dest[i] = '\0';
-	return (i);
+	while (src[x] != '\0')
+		x++;
+	return (x);
 }
