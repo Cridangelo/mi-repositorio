0a56ef899e992d6593d14963e093d3fb9cc4bc74
Cridangelo <cristian@DESKTOP-MKA3OST>
2024-11-12 15:39:30 +0100

    gnl

--- GNl/get_next_line.c
+++ GNl/get_next_line.c
@@ -1,16 +1,108 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*   gnl.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: acoayo-f <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: acoayo-f <acoayo-f@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/06 17:41:39 by acoayo-f          #+#    #+#             */
-/*   Updated: 2024/10/06 17:41:43 by acoayo-f         ###   ########.fr       */
+/*   Created: 2024/11/12 00:44:42 by acoayo-f          #+#    #+#             */
+/*   Updated: 2024/11/12 01:18:30 by acoayo-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char *get_next_line(int fd)
+#include "get_next_line.h"
+
+static char	*clean_storage(char *storage)
 {
-    
+	char	*new_storage;
+	char	*ptr;
+
+	ptr = ft_strchr(storage, '\n');
+	if (!ptr || !*(ptr + 1))
+		return (free(storage), NULL);
+	new_storage = ft_strdup(ptr + 1);
+	free(storage);
+	return (new_storage);
 }
+
+static char	*new_line(char *storage)
+{
+	char	*line;
+	char	*ptr;
+	int		len;
+
+	ptr = ft_strchr(storage, '\n');
+	len = (ptr - storage) + 1;
+	line = ft_substr(storage, 0, len);
+	if (!line)
+		return (NULL);
+	return (line);
+}
+
+static char	*read_buffer(int fd, char *storage)
+{
+	ssize_t	rid;
+	char	*buffer;
+	char	*temp;
+
+	buffer = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (!buffer)
+		return (free(storage), NULL);
+	rid = read(fd, buffer, BUFFER_SIZE);
+	while (rid > 0)
+	{
+		buffer[rid] = '\0';
+		temp = ft_strjoin(storage, buffer);
+		storage = temp;
+		if (ft_strchr(buffer, '\n'))
+			break ;
+		rid = read(fd, buffer, BUFFER_SIZE);
+	}
+	free(buffer);
+	if (rid == -1)
+		return (free(storage), NULL);
+	return (storage);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*storage = NULL;
+	char		*line;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	if (!storage || !ft_strchr(storage, '\n'))
+		storage = read_buffer(fd, storage);
+	if (!storage)
+		return (NULL);
+	line = new_line(storage);
+	if (!line)
+		return (free(storage), storage = NULL, NULL);
+	storage = clean_storage(storage);
+	return (line);
+}
+
+//#include <stdio.h>
+//#include <fcntl.h>
+//#include "get_next_line.h"
+//int main(void)
+//{
+//    int fd;
+//    char *line;
+//
+//  fd = open("txt.txt", O_RDONLY);
+// if (fd == -1)
+// {
+//    perror("Error al abrir el archivo");
+//   return (1);
+// }
+//
+//  while ((line = get_next_line(fd)) != NULL)
+// {
+//    printf("%s", line); 
+//   free(line);        
+//}
+//
+//  close(fd);
+//return (0);
+//}
--- GNl/get_next_line.h
+++ GNl/get_next_line.h
@@ -14,7 +14,7 @@
 # define GET_NEXT_LINE_H
 
 # ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 1
+#  define BUFFER_SIZE 42
 # endif
 # include <stdlib.h>
 # include <unistd.h>
@@ -23,9 +23,9 @@
 # include <fcntl.h>
 
 size_t	ft_strlen(const char *s);
-char	*ft_strjoin(char const *s1, char const *s2);
-char	*ft_strchr(const char *str, int c);
-char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strjoin(char *s1, char *s2);
+char	*ft_strchr(char *s, int c);
+char	*ft_substr(char *s, unsigned int start, size_t len);
 char	*get_next_line(int fd);
 char	*ft_strdup(const char *str);
 
