6869de1c6ba88738a548fe97473492ffbf8e9e1f
Cridangelo <163170024+Cridangelo@users.noreply.github.com>
2024-11-28 20:00:11 +0100

    Update ft_lstlast.c

--- pruebas/ft_lstlast.c
+++ pruebas/ft_lstlast.c
@@ -16,3 +16,27 @@ t_list *ft_lstlast(t_list *lst)
         lst = lst->next;
     return(lst);
 }
+// t_list *ft_lstnew(void *content)
+// {
+// 	t_list	*new_node;
+// 	new_node = (t_list *)malloc(sizeof(t_list)); 
+// 	if (!new_node)
+// 		return (NULL);
+// 	new_node->content = content;
+// 	new_node->next = NULL;
+// 	return (new_node);
+// }
+
+// int main()
+// {
+//     t_list *node1 = ft_lstnew("paco");
+//     t_list *node2 = ft_lstnew("pedro");
+//     t_list *node3 = ft_lstnew("pepe");
+
+//     node1->next = node2;
+//     node2->next = node3;
+//     node3->next = NULL;
+
+//     printf("%s \n", (char *)ft_lstlast(node1)->content);
+//     return (0);
+// }
