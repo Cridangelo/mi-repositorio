bcf056daccfc7f524f118f422220f942488fd607
Cridangelo <163170024+Cridangelo@users.noreply.github.com>
2024-11-28 20:00:36 +0100

    Update ft_lstsize.c

--- pruebas/ft_lstsize.c
+++ pruebas/ft_lstsize.c
@@ -21,3 +21,25 @@ int ft_lstsize(t_list *lst)
     
     return (count);
 }
+
+// t_list *ft_lstnew(void *content)
+// {
+// 	t_list	*new_node;
+// 	new_node = (t_list *)malloc(sizeof(t_list)); 
+// 	if (!new_node)
+// 		return (NULL);
+// 	new_node->content = content;
+// 	new_node->next = NULL;
+// 	return (new_node);
+// }
+
+// int main()
+// {
+//     t_list *node1 = ft_lstnew("paco");
+//     t_list *node2 = ft_lstnew("pedro");
+
+//     node1->next = node2;
+//     node2->next = NULL;
+//     printf("%d \n", ft_lstsize(node1));
+//     return (0);
+// }
